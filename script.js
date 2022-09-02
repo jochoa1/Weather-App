2fc3ab918d37d72451d9d70d2aefc6c429519d26
jochoa1 <ochoa8587@gmail.com>
2022-09-02 16:45:22 -0700

    display alert when invalid city is entered

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid="+ this.apiKey
         )
-        .then((response) => response.json())
+        .then((response) => {
+            if (!response.ok) {
+                alert("No weather found!");
+                throw new Error("No weather found.");
+            }
+            return response.json();
+        })
         .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
